9dc26934908465a8436839a8b31735064f63e441
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-27 05:25:18 +0700

    Update AWS region environment variable in client initialization

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -3,6 +3,6 @@ import os
 import boto3
 
 
-client = boto3.client("sqs", endpoint_url=os.environ.get("SQS_ENDPOINT"), region_name=os.environ.get("AWS_REGION"))
+client = boto3.client("sqs", endpoint_url=os.environ.get("SQS_ENDPOINT"), region_name=os.environ.get("AWS_DEFAULT_REGION"))
 
 train_task_queue_url = client.get_queue_url(QueueName="training-task-queue")["QueueUrl"]
